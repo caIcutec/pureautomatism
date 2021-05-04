efc3d221568a6d5f4ce2410f5d2b140d339b2b09
caIcutec <caIcutec@github.com>
2021-05-03 17:20:20 -0700

    doing stuff

--- app.py
+++ app.py
@@ -5,9 +5,8 @@ app = Flask(__name__)
 
 # A welcome message to test our server
 @app.route('/')
-def app():
+def script():
     return sp.getscript()
-
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
     app.run(threaded=True, port=5000)
