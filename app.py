b4f9537bbd52d4c615b31f15aff3e1b1715ecab0
caIcutec <caIcutec@github.com>
2021-05-03 17:27:14 -0700

    hi

--- app.py
+++ app.py
@@ -7,6 +7,12 @@ app = Flask(__name__)
 @app.route('/')
 def script():
     return sp.getscript()
+@app.route('/hi')
+def script():
+    return sp.getscript()
+@app.route('/bruh')
+def script():
+    return "<h1>bruh</h1>"
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
     app.run(threaded=True, port=5000)
